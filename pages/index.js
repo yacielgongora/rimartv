415f26825630ac5ff580b6d5718d8a14883b4779
yasiel.gongora <yasiel.gongora@gmail.com>
2022-07-08 15:10:14 -0400

    Use ssl certificate for video player

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import { Fragment } from 'react';
 import Hero from '../components/UI/Hero';
 import Services from '../components/UI/Services';
-import About from '../components/UI/About';
-import Portfolio from '../components/UI/Portfolio';
 import Testimonial from '../components/UI/Testimonial';
 import Contact from '../components/UI/Contact';
 
 export default function Home() {
   return (
     <Fragment>
-      <Hero src="//rimartv.sentidocomun.do/hls/stream.m3u8" />
+      <Hero src="https://rimartv.ddns.net/hls/stream.m3u8" />
       <Services />
       <Testimonial />
       <Contact />
